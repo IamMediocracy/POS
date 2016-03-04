9365542e576c4c1135d6192d475b34c6e9a5a256
Zack <znh8089@lhup.edu>
2016-03-04 12:56:06 -0500

    Update DB.java
    Casey accidentally pushed the DB.java class with credentials. Old credentials so they don't matter until new DB is up and running anyway. I replaced with placeholder tags.

--- src/POS_classes/DB.java
+++ src/POS_classes/DB.java
@@ -9,9 +9,9 @@ import java.sql.Statement;
 public class DB {
 
 	protected Connection conn;
-	private String url = "jdbc:mysql://comp305pos.lhup.edu:3306/pos";
-	private String user = "root";
-	private String pass = "Rand0mcomp305";
+	private String url = "<JDBC URL>";
+	private String user = "<USERNAME>";
+	private String pass = "<PASSWORD>";
 
 	protected DB() {
 
