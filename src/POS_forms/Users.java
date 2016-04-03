c5f0269291479ab788ffd77cdb734e989e40ed6d
marshall <mal4306@lhup.edu>
2016-04-03 15:31:21 -0400

    added a bit more to the insert statement to add cashier, need help

--- src/POS_forms/Users.java
+++ src/POS_forms/Users.java
@@ -3,13 +3,17 @@ package POS_forms;
 import POS_classes.UIPanels;
 
 public class Users extends UIPanels {
+	public Users() {
+	}
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7686999830925783105L;
 
 	public void addCashier() {
-		//PreparedStatement addCashier = DB.conn.prepareStatement("");
+		//DB DB = new DB();
+		//PreparedStatement addCashier = DB.conn.prepareStatement
+				//("select usr_id, usr_fname, usr_lname, usr_cashier from user where usr_cashier = true");
 	}
 
 	@Override
