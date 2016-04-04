c9dccc8da680b817d864cf20ae1e3a11919ec8f5
Rick <Rick@Rick-PC>
2016-04-03 20:40:28 -0400

    added rs2xml class and updated transfer page

--- src/POS_forms/Transfer.java
+++ src/POS_forms/Transfer.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -15,12 +16,19 @@ import javax.swing.SwingConstants;
 import POS_classes.DB;
 import POS_classes.DBTableModel;
 import POS_classes.UIPanels;
+import net.proteanit.sql.DbUtils;
 
 public class Transfer extends UIPanels {
 
 	Connection connection = null;
 
 	public Transfer() {
+		tablepane.setBounds(111, 5, 2, 2);
+		pnl_table.setLayout(null);
+		
+		table= new JTable();
+		table.setBounds(10, 5, 205, 134);
+		pnl_table.add(table);
 
 		DB DB;
 		try {
@@ -34,12 +42,15 @@ public class Transfer extends UIPanels {
 				public void actionPerformed(ActionEvent arg0) {
 
 					try {
-						String query = "";
+						String query = "update running_totals set money_total = money_total + ? WHERE location_name ='drawer1' "
+								+ "UNION update running_totals set money_total = money_total - ? WHERE location_name ='safe' ";
 						PreparedStatement pst = connection.prepareStatement(query);
-						ResultSet rs = pst.executeQuery();
-						table.setModel(model);
+						
+						JOptionPane.showMessageDialog(null, "Funds withdrawn from Safe");
+						
+						
 						pst.close();
-						rs.close();
+						
 
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
@@ -59,13 +70,14 @@ public class Transfer extends UIPanels {
 				public void actionPerformed(ActionEvent e) {
 
 					try {
-						String query = "";
+						String query = "update running_totals set money_total = money_total - ? WHERE location_name ='drawer1' "
+								+ "UNION update running_totals set money_total = money_total + ? WHERE location_name ='safe' ";
 						PreparedStatement pst;
 						pst = connection.prepareStatement(query);
-						ResultSet rs = pst.executeQuery();
-						table.setModel(model);
+						
+						JOptionPane.showMessageDialog(null, "Funds deposited to Safe");
 						pst.close();
-						rs.close();
+						
 					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -82,25 +94,24 @@ public class Transfer extends UIPanels {
 			btnViewCurrentAmounts = new JButton("View Current Amounts");
 			btnViewCurrentAmounts.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-
+					
+				try{	
 					String query = "select * from running_totals";
 					PreparedStatement pst = connection.prepareStatement(query);
 					ResultSet rs = pst.executeQuery();
 					table.setModel(DbUtils.resultSetToTableModel(rs));
 					pst.close();
 					rs.close();
-
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				}
 			});
+			
 			buttons_panel.add(btnViewCurrentAmounts);
-
-			table = new JTable();
-			pnl_table.add(table);
-
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		
+		
 	}
 
 	private static final long serialVersionUID = 6047803356240229042L;
@@ -110,6 +121,7 @@ public class Transfer extends UIPanels {
 	private JTextField depositAMT;
 	private JButton btnViewCurrentAmounts;
 	private JTable table;
+	
 
 	@Override
 	public void setTableInfo() throws ClassNotFoundException {
