1b35159ae70f30ca874cdf620d879ec802e65f76
Zachary Heins <zack@low77.com>
2016-04-24 21:10:23 -0400

    Fixed updating table after withdraw/deposit

--- src/POS_forms/Transfer.java
+++ src/POS_forms/Transfer.java
@@ -22,7 +22,7 @@ public class Transfer extends UIPanels {
 		DB DB;
 		try {
 			DB = new DB();
-			
+
 			JButton btnViewCurrentAmounts = new JButton("View Current Amounts");
 			JButton btnWithdrawl = new JButton("Withdrawal");
 			btnWithdrawl.setVerticalAlignment(SwingConstants.TOP);
@@ -31,10 +31,10 @@ public class Transfer extends UIPanels {
 				public void actionPerformed(ActionEvent arg0) {
 
 					try {
-						
-						String query = "update running_totals set money_total = money_total + "
-								+ withdrawlAMT.getText() + " WHERE location_name ='drawer1' ";
-						String query2 =	"update running_totals set money_total = money_total - "
+
+						String query = "update running_totals set money_total = money_total + " + withdrawlAMT.getText()
+								+ " WHERE location_name ='drawer1' ";
+						String query2 = "update running_totals set money_total = money_total - "
 								+ withdrawlAMT.getText() + " WHERE location_name ='safe' ";
 						PreparedStatement pst = DB.conn.prepareStatement(query);
 						pst.execute();
@@ -43,19 +43,20 @@ public class Transfer extends UIPanels {
 						JOptionPane.showMessageDialog(null, "Funds withdrawn from Safe");
 
 						pst.close();
+						resetTable();
 
 					} catch (SQLException e) {
-						
+
 						JOptionPane.showMessageDialog(null, "Error: Please Check Withdrawal Amount");
-						
+
 						e.printStackTrace();
 					}
 
 				}
 			});
-			
-//			buttons_panel.add(btnViewCurrentAmounts);
-			
+
+			// buttons_panel.add(btnViewCurrentAmounts);
+
 			buttons_panel.add(btnWithdrawl);
 
 			withdrawlAMT = new JTextField();
@@ -71,20 +72,21 @@ public class Transfer extends UIPanels {
 				public void actionPerformed(ActionEvent e) {
 
 					try {
-						
+
 						String query = "update running_totals set money_total = money_total - " + depositAMT.getText()
 								+ " WHERE location_name ='drawer1' ";
-								
-						String query2 =	"update running_totals set money_total = money_total + " + depositAMT.getText()
+
+						String query2 = "update running_totals set money_total = money_total + " + depositAMT.getText()
 								+ " WHERE location_name ='safe' ";
 						PreparedStatement pst;
-						
+
 						pst = DB.conn.prepareStatement(query);
 						pst.execute();
 						pst = DB.conn.prepareStatement(query2);
 						pst.execute();
 						JOptionPane.showMessageDialog(null, "Funds deposited to Safe");
 						pst.close();
+						resetTable();
 
 					} catch (SQLException e1) {
 						JOptionPane.showMessageDialog(null, "Error: Please Check Deposit Amount");
@@ -99,33 +101,6 @@ public class Transfer extends UIPanels {
 			buttons_panel.add(depositAMT);
 			depositAMT.setColumns(10);
 
-			
-			btnViewCurrentAmounts.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-
-					try {
-						
-							SelectBuilder sqlBuilder = new SelectBuilder().column("Money_total").column("location_name")
-									.from("running_totals").where("location_name = 'safe' OR location_name = 'drawer1'");
-							PreparedStatement pst = DB.conn.prepareStatement(sqlBuilder.toString());
-							executeQuery(pst);
-							setTableInfo();
-						
-						JOptionPane.showMessageDialog(null, "Worked");
-						pst.close();
-						
-					//	rs.close();
-						
-					} catch (SQLException e1) {
-						JOptionPane.showMessageDialog(null, "Error: Cannot Connect to Database");
-						e1.printStackTrace();
-					} catch (ClassNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				}
-			});
-			
 			SelectBuilder sqlBuilder = new SelectBuilder().column("Money_total").column("location_name")
 					.from("running_totals").where("location_name = 'safe' OR location_name = 'drawer1'");
 			PreparedStatement pst = DB.conn.prepareStatement(sqlBuilder.toString());
@@ -133,11 +108,37 @@ public class Transfer extends UIPanels {
 			super.setTableInfo();
 			pst.close();
 
-		} catch(SQLException | ClassNotFoundException e2) {
+		} catch (SQLException | ClassNotFoundException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
+
+	}
+
+	public void resetTable() {
 		
+		try {
+			
+			DB DB = new DB();
+
+			SelectBuilder sqlBuilder = new SelectBuilder().column("Money_total").column("location_name")
+					.from("running_totals").where("location_name = 'safe' OR location_name = 'drawer1'");
+			PreparedStatement pst = DB.conn.prepareStatement(sqlBuilder.toString());
+			executeQuery(pst);
+			setTableInfo();
+
+			JOptionPane.showMessageDialog(null, "Worked");
+			pst.close();
+
+			// rs.close();
+
+		} catch (SQLException e1) {
+			JOptionPane.showMessageDialog(null, "Error: Cannot Connect to Database");
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	private static final long serialVersionUID = 6047803356240229042L;
